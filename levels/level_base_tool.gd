80321d3fa1e2ec2d16a1858b740a852892d477ff
Telkin1 <229523731+SolanumEcho@users.noreply.github.com>
2026-01-31 19:01:05 +0100

    Level 3

--- levels/level_base_tool.gd
+++ levels/level_base_tool.gd
@@ -5,12 +5,8 @@ extends Node2D
 @export_tool_button("Snap elements to grid", "Callable") var snap_action = _snap_elements_to_grid
 
 
-
-
 func _ready():
 	_snap_elements_to_grid()
-
-
 
 
 func _snap_elements_to_grid():
