28ecbaa6eb70e7fe53293863914ddf9c4fc99c75
Marco Benedetti <cairnvale@proton.me>
2026-01-31 14:57:26 +0100

    feat: add restart level function

--- main_stage/main_stage.gd
+++ main_stage/main_stage.gd
@@ -10,7 +10,9 @@ const COLLISION_LAYER_WATER: int = 3
 const COLLISION_LAYER_BOULDER: int = 4
 const COLLISION_LAYER_CHASM: int = 5
 const COLLISION_LAYER_HEDGE: int = 6
+const RESTART_DELAY: float = 0.4 # seconds
 
+var restart_timer: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,7 +21,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("restart_level"):
+		restart_timer += delta
+	else:
+		restart_timer = 0.0
+		
+	if restart_timer > RESTART_DELAY:
+		get_tree().reload_current_scene()
+
 
 func new_game():
 	$Player.snap_to_tiles($StartPosition.position)
