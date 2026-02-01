4d09985cd3d80b031bdba730f87b07fc265e9f82
Marco Benedetti <cairnvale@proton.me>
2026-02-01 08:46:02 +0100

    fix: smooth following camera and add threshold

--- player/following_camera.gd
+++ player/following_camera.gd
@@ -9,6 +9,6 @@ const SPEED: float = 2000
 
 func _process(_delta: float) -> void:
 	var diff = target.position - position
-	velocity = diff.normalized() * SPEED
+	velocity = diff.normalized() * SPEED if diff.length() > $Camera2D.position_smoothing_speed else Vector2.ZERO
 	
 	move_and_slide()
